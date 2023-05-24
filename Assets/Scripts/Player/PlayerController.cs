b2d6a5abe747b418405e86f3d1253dd4032b31c6
Paullo B. Felipe <paullotsw@gmail.com>
2023-05-23 21:18:40 -0300

    basic animation is done;

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,9 +5,13 @@ using Player.Movement;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] SpriteRenderer playerSprite;
     public Rigidbody2D rb;
-    private bool canMove;
-
+    [SerializeField] Animator playerAnimator;
+    private bool canMove = true;
+    private bool isDefaultSkin;
+    [SerializeField] List<Sprite> skinListSprites = new();
+    private int selectedSkin;
 
 
     private void FixedUpdate()
@@ -22,5 +26,22 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         playerMovement.Move(moveX, moveY);
+        playerAnimator.SetFloat("moveX", moveX);
+        playerAnimator.SetFloat("moveY", moveY);
+        if (moveX != 0 || moveY != 0)
+            return;
+        playerAnimator.SetBool("isCharacterMoving", false);
+    }
+
+    private void ChangePlayerSkin()
+    {
+        switch (selectedSkin)
+        {
+            case 0:
+                //play particle effect
+                playerSprite.sprite = skinListSprites[0];
+                playerAnimator.SetInteger("playerSkin", selectedSkin);
+                break;
+        }
     }
 }
